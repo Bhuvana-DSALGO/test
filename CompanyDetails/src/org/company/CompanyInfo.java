4ffca98b9bce2c7678ddff83fc1f7e56b50ffcaf
Bhuvana-DSALGO <bhuvanasudha85@gmail.com>
2023-07-06 14:46:26 -0400

    first commit

---
+++ CompanyDetails/src/org/company/CompanyInfo.java
@@ -0,0 +1,42 @@
+package org.company;
+
+public class CompanyInfo {
+	
+	public void companyName() {
+	
+		String A = ("Numpy Ninja");
+		System.out.println("Company Name:"+A);
+		
+	}
+	
+public void companyId() {
+	
+	String i = ("SDET 109");
+	System.out.println("CompanyID:"+i);
+		
+	}
+	
+public void companyAdd() {
+	
+	String B = ("DE,USA");
+	System.out.println("CompanyAdd:"+B);
+	
+}
+	
+public static void main(String[] args) {
+	
+	CompanyInfo C= new CompanyInfo();
+	C.companyName();
+	C.companyId();
+	C.companyAdd();
+	
+		
+}	
+	
+	
+	
+	
+	
+	
+
+}
